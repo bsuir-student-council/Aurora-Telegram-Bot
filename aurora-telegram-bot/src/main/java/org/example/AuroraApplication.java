9b835cc3a7763ed2ebc384228a20b89a34756b46
Daniil-Tsiunchyk <fcad.td@gmail.com>
2024-08-20 02:21:23 +0300

    refactor: Move configuration to .env file for environment-based setup

--- aurora-telegram-bot/src/main/java/org/example/AuroraApplication.java
+++ aurora-telegram-bot/src/main/java/org/example/AuroraApplication.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class AuroraApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         SpringApplication.run(AuroraApplication.class, args);
     }
 }
